4888eeb6acc1d1d0e76d49a2cbbee8ae09e6afac
Boyzmsc <boyzmsc@kookmin.ac.kr>
2021-09-26 15:23:37 +0900

    Upload avltree skeleton

---
+++ avltree/avltree_test.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "avltree.h"
+
+void print(const int key, int value) { printf("%d ", key); }
+
+int main()
+{
+    const int testcases[] = {40, 11, 77, 33, 20, 90, 99, 70, 88, 80, 66, 10, 22, 30, 44, 55, 50, 60, 25, 49};
+
+    struct avl_node *tree = NULL;
+
+    for (const int *it = testcases; it < testcases + sizeof(testcases) / sizeof(int); ++it)
+    {
+        avl_insert(&tree, *it, 0);
+        avl_inorder(tree, print);
+        printf("\n");
+    }
+    for (const int *it = testcases; it < testcases + sizeof(testcases) / sizeof(int); ++it)
+    {
+        avl_erase(&tree, *it);
+        avl_inorder(tree, print);
+        printf("\n");
+    }
+    for (const int *it = testcases; it < testcases + sizeof(testcases) / sizeof(int); ++it)
+    {
+        avl_insert(&tree, *it, 0);
+        avl_inorder(tree, print);
+        printf("\n");
+    }
+    for (const int *it = testcases + sizeof(testcases) / sizeof(int) - 1; testcases <= it; --it)
+    {
+        avl_erase(&tree, *it);
+        avl_inorder(tree, print);
+        printf("\n");
+    }
+}
